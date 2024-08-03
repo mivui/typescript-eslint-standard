7137dfa230bb1a02b7720afa518ea95efc9796f5
mivui <lv_xiao_bu@qq.com>
2024-08-03 10:46:09 +0800

    no-unsafe-return off

--- lib/index.ts
+++ lib/index.ts
@@ -216,6 +216,7 @@ export const typescriptRules: TSESLint.FlatConfig.Rules = {
   '@typescript-eslint/no-unsafe-assignment': 'off',
   '@typescript-eslint/no-unsafe-enum-comparison': 'off',
   '@typescript-eslint/no-unsafe-member-access': 'off',
+  '@typescript-eslint/no-unsafe-return': 'off',
   '@typescript-eslint/no-unused-expressions': [
     'error',
     {
