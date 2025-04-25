4f6e6e8d13d9e8fff199b0f4a5c7f4513166f27b
mivui <49021590+mivui@users.noreply.github.com>
2025-04-25 19:22:47 +0800

    no-unsafe-call

--- eslint.config.js
+++ eslint.config.js
@@ -212,6 +212,7 @@ const typescriptRules = {
   '@typescript-eslint/no-unnecessary-type-parameters': 'off',
   '@typescript-eslint/no-unsafe-argument': 'off',
   '@typescript-eslint/no-unsafe-assignment': 'off',
+  '@typescript-eslint/no-unsafe-call': 'off',
   '@typescript-eslint/no-unsafe-enum-comparison': 'off',
   '@typescript-eslint/no-unsafe-member-access': 'off',
   '@typescript-eslint/no-unsafe-return': 'off',
--- lib/index.ts
+++ lib/index.ts
@@ -214,6 +214,7 @@ export const typescriptRules: TSESLint.FlatConfig.Rules = {
   '@typescript-eslint/no-unnecessary-type-parameters': 'off',
   '@typescript-eslint/no-unsafe-argument': 'off',
   '@typescript-eslint/no-unsafe-assignment': 'off',
+  '@typescript-eslint/no-unsafe-call': 'off',
   '@typescript-eslint/no-unsafe-enum-comparison': 'off',
   '@typescript-eslint/no-unsafe-member-access': 'off',
   '@typescript-eslint/no-unsafe-return': 'off',
