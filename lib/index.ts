a579289e1b693d3040e5b19acb843df66c7e0371
mivui <49021590+mivui@users.noreply.github.com>
2025-07-27 09:32:50 +0800

    rule: any off

--- lib/index.ts
+++ lib/index.ts
@@ -127,6 +127,7 @@ export const eslintRules: TSESLint.FlatConfig.Rules = {
 };
 
 export const typescriptRules: TSESLint.FlatConfig.Rules = {
+  '@typescript-eslint/no-explicit-any': 'off',
   '@typescript-eslint/adjacent-overload-signatures': 'error',
   '@typescript-eslint/array-type': ['error', { default: 'array-simple' }],
   '@typescript-eslint/ban-tslint-comment': 'error',
